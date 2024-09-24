a1f043a3e82954fa273a0fde349475461e16a55a
Marcus Walker <80381071+G00Dway@users.noreply.github.com>
2024-09-24 12:04:58 +0400

    Update Aimbot.lua

--- Aimbot.lua
+++ Aimbot.lua
@@ -103,14 +103,14 @@ getgenv().ExunysDeveloperAimbot = {
 		AliveCheck = true,
 		WallCheck = false,
 
-		OffsetToMoveDirection = false,
-		OffsetIncrement = 15,
+		OffsetToMoveDirection = false, -- Prediction
+		OffsetIncrement = 15, -- Min: 1; Max: 30 -- Amplitude
 
-		Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity = 0, -- Animation length (in seconds) before fully locking onto target / CFrame Sensitivity
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
 
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
-		LockPart = "Head", -- Body part to lock on
+		LockPart = "Torso", -- Body part to lock on
 
 		TriggerKey = Enum.UserInputType.MouseButton2,
 		Toggle = false
@@ -120,23 +120,19 @@ getgenv().ExunysDeveloperAimbot = {
 		Enabled = true,
 		Visible = true,
 
-		Radius = 90,
+		Radius = 120, -- Field Of View
 		NumSides = 60,
 
-		Thickness = 1,
+		Thickness = 2,
 		Transparency = 1,
 		Filled = false,
 
 		RainbowColor = false,
 		RainbowOutlineColor = false,
-		Color = Color3fromRGB(255, 255, 255),
-		OutlineColor = Color3fromRGB(0, 0, 0),
-		LockedColor = Color3fromRGB(255, 150, 150)
-	},
-
-	Blacklisted = {},
-	FOVCircle = Drawingnew("Circle"),
-	FOVCircleOutline = Drawingnew("Circle")
+		Color = Color3.fromRGB(255, 255, 255),
+		OutlineColor = Color3.fromRGB(0, 0, 0),
+		LockedColor = Color3.fromRGB(255, 150, 150)
+	}
 }
 
 local Environment = getgenv().ExunysDeveloperAimbot
